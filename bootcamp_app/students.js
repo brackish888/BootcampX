60308d38f333f8a24d3b5be3a811903d8eaa3f2e
Sam T <bbrackish@protonmail.com>
2021-04-02 00:39:58 +0000

    update students and teachers with parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${process.argv[2]}%'
-LIMIT ${process.argv[3]};`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     for (const student of res.rows)
       console.log(`${student.name} has an id of ${student.id} and was in the ${student.cohort} cohort`);
